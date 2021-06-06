dcf38feea6db7b0f1d5b0e757bdb59e0cb2bfb94
Jhon Carlos Acevedo Mendoza <jhonacevedomendoza@gmail.com>
2021-06-06 18:27:09 -0500

    new functions

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -2,18 +2,32 @@ import React, {useEffect, useState} from 'react';
 import Gif from './Gif'
 import getGifs from '../services/getGifs'
 
-export default function ListOfGifs ({keyword}) {
-    const [gifs, setGifs] = useState([]);
+export default function ListOfGifs ({params}) {
+    const {keyword} = params
+    const [loading, setLoading] = useState(false)
+
+    const [gifs, setGifs] = useState([])
 
     useEffect(function () {
-        getGifs({ keyword }).then(gifs => setGifs(gifs))
+        setLoading(true)
+        getGifs({ keyword })
+        .then(gifs =>{
+            setGifs(gifs)
+            setLoading(false)
+        })
     }, [keyword])
 
-    return gifs.map(({id, title, url}) =>
-        <Gif
-            id={id}  
-            key={id}
-            title={title}
-            url={url}
-        ></Gif>)
+    if(loading) return <i>🧠Cargando...🧠</i>
+
+    return <div>
+        {
+        gifs.map(({id, title, url}) =>
+            <Gif
+                id={id}  
+                key={id}
+                title={title}
+                url={url}
+            ></Gif>)
+        }
+    </div>
 }
--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,7 +1,10 @@
+import { useState } from "react"
+
 const apiKey = '1eG46qAktzUTYYG4ChP5BPXmuIq8JS3S'
 
 export default function getGifs ({keyword = 'berserk'} = {}) {
     const APIURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=4&offset=0&rating=g&lang=en`
+
     return fetch(APIURL)
         .then(res => res.json())
         .then(response => {
